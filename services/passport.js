c1af6ee233e00c5b8f7b0f2ddf281ca917781fd7
Evgenii Dedushkevich <kox.deda@yandex.ru>
2020-05-23 20:52:00 +0300

    Auth complete

---
+++ services/passport.js
@@ -0,0 +1,47 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users1');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+});
+
+
+passport.use(
+    new GoogleStrategy(
+      {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+      },
+      (accessToken, refreshToken, profile, done) => {
+        User.findOne({ googleId: profile.id})
+          .then((existingUser) => {
+            if (existingUser){
+              // we have a user with this Id
+              done(null, existingUser);
+            } else {
+              // need to create a new record
+              new User({
+                googleId: profile.id })
+                .save()
+                .then(user => done(null, user));
+            }
+          })
+
+
+
+
+      }
+    )
+  );
