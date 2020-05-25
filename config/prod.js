f70a0d1f2fd51ffaf9b22fff07006e67e5221330
Evgenii Dedushkevich <kox.deda@yandex.ru>
2020-05-25 20:22:17 +0300

    finished auth flow2

--- config/prod.js
+++ config/prod.js
@@ -13,7 +13,7 @@ module.exports = {
 
 
 // dev
-   // mongodb+srv://koxdeda:<password>@cluster0-afcvz.gcp.mongodb.net/test?retryWrites=true&w=majority prod
+   // mongodb+srv://koxdeda:passwordpassword@cluster0-afcvz.gcp.mongodb.net/test?retryWrites=true&w=majority prod
 
 
  //  1037198122538-9nd5uas1ngq2f6de7ei13kofhmapnuqq.apps.googleusercontent.com
