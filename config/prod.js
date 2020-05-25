1b3b210f88b5314902dcc1472cf9e5654c60f734
Evgenii Dedushkevich <kox.deda@yandex.ru>
2020-05-25 20:11:31 +0300

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,20 @@
+// prod.js - production keys here!!
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI, 
+    cookieKey: process.env.COOKIE_KEY
+}
+
+
+
+
+
+
+
+// dev
+   // mongodb+srv://koxdeda:<password>@cluster0-afcvz.gcp.mongodb.net/test?retryWrites=true&w=majority prod
+
+
+ //  1037198122538-9nd5uas1ngq2f6de7ei13kofhmapnuqq.apps.googleusercontent.com
+ // Gkc5vGpLq-IY-Z6hJLsEJLvF
